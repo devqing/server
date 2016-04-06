b45b33326be599242da475dfc09a2f9da203e3b3
liuweiqing <liuweiqing@kuaikuaiyu.com>
2016-04-06 15:53:34 +0800

    delete print

--- liuweiqing_server/handlers/api/SignUp.py
+++ liuweiqing_server/handlers/api/SignUp.py
@@ -39,25 +39,25 @@ class SignUp(RequestHandler):
 			raise error.AccountAlreadyExists()
 		account = yield self.user_model.GetUserFromMobile(mobile)
 		# print account['nike_name']
-		# uid = str(account['_id'])
-		# rongyun_token = rongyun_client.user_get_token(
-		# 	uid,
-		# 	account['nike_name'],
-		# 	'http://www.rongcloud.cn/images/logo.png'
-		# )
-		# server_token = ''.join([random.choice(string.digits + string.letters) for _ in range(32)])
-		# tid = yield CreatUserToken().Action(uid, server_token, rongyun_token['token'])
-		# print tid
-		# token_result = yield self.token_model.GetTokenFromTid(tid)
-		# print token_result['server_token']
-		# result = {
-		# 	'_id':str(account['_id']),
-		# 	'nike_name':account['nike_name'],
-		# 	'server_token':token_result['server_token'],
-		# 	'token':token_result['rongyun_token']
-		# }
+		uid = str(account['_id'])
+		rongyun_token = rongyun_client.user_get_token(
+			uid,
+			account['nike_name'],
+			'http://www.rongcloud.cn/images/logo.png'
+		)
+		server_token = ''.join([random.choice(string.digits + string.letters) for _ in range(32)])
+		tid = yield CreatUserToken().Action(uid, server_token, rongyun_token['token'])
+		print tid
+		token_result = yield self.token_model.GetTokenFromTid(tid)
+		print token_result['server_token']
+		result = {
+			'_id':str(account['_id']),
+			'nike_name':account['nike_name'],
+			'server_token':token_result['server_token'],
+			'token':token_result['rongyun_token']
+		}
 
-		self.render({'11':'2222'})
+		self.render(result)
 
 
 """
