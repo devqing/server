ff36255d8b7aa8238ae47ef22090b8ec3e2bcdba
liuweiqing <liuweiqing@kuaikuaiyu.com>
2016-04-06 15:31:58 +0800

    update

--- liuweiqing_server/app.py
+++ liuweiqing_server/app.py
@@ -18,7 +18,7 @@ import tornado.options
 import tornado.web
 from tornado.options import define, options
 from urls import urls
-
+import logging
 
 define("port", default=1880, help="run on the given port", type=int)
 
@@ -29,6 +29,7 @@ def main():
         urls,
         template_path=os.path.join(os.path.dirname(__file__), "templates")
     )
+	logging.info("**Request to MainHandler!")
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
--- liuweiqing_server/handlers/api/SignUp.py
+++ liuweiqing_server/handlers/api/SignUp.py
@@ -17,7 +17,7 @@ from RequestHandler import RequestHandler
 from behaviors import CreatNewUser
 from behaviors import CreatUserToken
 import error
-
+import logging
 
 app_key = "8brlm7ufrnrx3"
 app_secret = "ssZ9TDw2v7"
@@ -40,24 +40,24 @@ class SignUp(RequestHandler):
 		account = yield self.user_model.GetUserFromMobile(mobile)
 		print account['nike_name']
 		uid = str(account['_id'])
-		# rongyun_token = rongyun_client.user_get_token(
-		# 	uid,
-		# 	account['nike_name'],
-		# 	'http://www.rongcloud.cn/images/logo.png'
-		# )
-		# server_token = ''.join([random.choice(string.digits + string.letters) for _ in range(32)])
-		# tid = yield CreatUserToken().Action(uid, server_token, rongyun_token['token'])
-		# print tid
-		# token_result = yield self.token_model.GetTokenFromTid(tid)
-		# print token_result['server_token']
-		# result = {
-		# 	'_id':str(account['_id']),
-		# 	'nike_name':account['nike_name'],
-		# 	'server_token':token_result['server_token'],
-		# 	'token':token_result['rongyun_token']
-		# }
+		rongyun_token = rongyun_client.user_get_token(
+			uid,
+			account['nike_name'],
+			'http://www.rongcloud.cn/images/logo.png'
+		)
+		server_token = ''.join([random.choice(string.digits + string.letters) for _ in range(32)])
+		tid = yield CreatUserToken().Action(uid, server_token, rongyun_token['token'])
+		print tid
+		token_result = yield self.token_model.GetTokenFromTid(tid)
+		print token_result['server_token']
+		result = {
+			'_id':str(account['_id']),
+			'nike_name':account['nike_name'],
+			'server_token':token_result['server_token'],
+			'token':token_result['rongyun_token']
+		}
 
-		self.render({'1':'11'})
+		self.render(result)
 
 
 """
