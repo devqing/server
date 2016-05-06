49d59d44802d95dfcd453059debe12dd46b83056
liuweiqing <liuweiqing@kuaikuaiyu.com>
2016-05-06 19:08:37 +0800

    update

--- liuweiqing_server/templates/customer.js
+++ liuweiqing_server/templates/customer.js
@@ -1,1 +1,15 @@
-var test = 'test'
+defineClass('AddressLocationViewController', {
+//  init: function() {
+//     self = self.super().init();
+////     require('CLLocationManager');
+////     var manager = CLLocationManager.alloc().init();
+////     manager.distanceFilter(50);
+////     manager.delegate(self);
+////     self.setLocationManager(manager);
+//
+//     return self;
+//  }
+    viewDidLoad: function() {
+//        self.super().viewDidLoad();
+     }
+})
